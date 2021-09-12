b37e71c0fd747aa4fc631e7638ae14bfb5cd7794
ritesh-chafer <riteshprasadsingh8@gmail.com>
2021-09-12 10:41:54 +0530

    server.py

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return 'Hello'
+
+if __name__ == "__main__":
+    print("starting flask server for home price prediction....")
+    app.run()
+
+app.run(debug=True)
